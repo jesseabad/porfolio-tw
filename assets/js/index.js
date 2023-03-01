03a176cfac2504a29462836f877fb6aa86b59aa7
jesseabad <jesse.abad@gmail.com>
2023-03-01 00:31:19 -0600

    created external js

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+/* Make dynamic date appear */
+(function () {
+    if (document.getElementById("get-current-year")) {
+        document
+            .getElementById("get-current-year")
+            .innerHTML = new Date().getFullYear();
+    }
+})();
+/* Function for opening navbar on mobile */
+function toggleNavbar(collapseID) {
+    document
+        .getElementById(collapseID)
+        .classList
+        .toggle("hidden");
+    document
+        .getElementById(collapseID)
+        .classList
+        .toggle("block");
+}
+/* Function for dropdowns */
+function openDropdown(event, dropdownID) {
+    let element = event.target;
+    while (element.nodeName !== "A") {
+        element = element.parentNode;
+    }
+    Popper.createPopper(
+        element,
+        document.getElementById(dropdownID),
+        {placement: "bottom-start"}
+    );
+    document
+        .getElementById(dropdownID)
+        .classList
+        .toggle("hidden");
+    document
+        .getElementById(dropdownID)
+        .classList
+        .toggle("block");
+}
