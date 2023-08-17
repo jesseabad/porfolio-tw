01272ec75498420b38c8c95025dd319fd9580c3e
Jesse Abad <jesse.abad@gmail.com>
2023-08-17 00:30:57 -0500

    updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,3 +37,9 @@ function openDropdown(event, dropdownID) {
         .classList
         .toggle("block");
 }
+function toggleModal(modalID){
+    document.getElementById(modalID).classList.toggle("hidden");
+    document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
+    document.getElementById(modalID).classList.toggle("flex");
+    document.getElementById(modalID + "-backdrop").classList.toggle("flex");
+  }
